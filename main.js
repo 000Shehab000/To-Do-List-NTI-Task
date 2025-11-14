69582592cc952d3f4cda9dec2fc1d1215b894f6d
000Shehab000 <shhab200025@gmail.com>
2025-11-14 16:19:28 +0200

    feat: add validation for empty todo input
    - Added alert message
    - Prevented empty todos
    - Cleared input field after adding

--- main.js
+++ main.js
@@ -4,10 +4,18 @@ const toDoContainer = document.getElementById('to-dos-container')
 
 function addToDo() {
   const toDoText = toDo.value
+
+  if (toDoText === '') {
+    alert('Please write a to do task!')
+    return
+  }
+
   const toDoCard = document.createElement('div')
   toDoCard.textContent = toDoText
 
   toDoContainer.appendChild(toDoCard)
+
+  toDo.value = '' // remove text after adding
 }
 
 addButton.addEventListener('click', addToDo)
